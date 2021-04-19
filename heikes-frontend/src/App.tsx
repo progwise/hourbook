cd6fc7d9bc76502b9d2c903451ad0e8353d49638
HeikeKruger <heikrueg@gmail.com>
2021-04-19 15:27:53 +0200

    2 new compinents added userInfo and datum

--- heikes-frontend/src/App.tsx
+++ heikes-frontend/src/App.tsx
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { UserInfo } from './components/userInfo';
+import { Datum } from './components/datum';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+    <UserInfo />
+    <Datum />
+  </>)
 }
 
 export default App;
---
+++ heikes-frontend/src/components/datum.tsx
@@ -0,0 +1,3 @@
+export const Datum = () => {
+    return <div>heute ist der 19.04.2021</div>
+}
