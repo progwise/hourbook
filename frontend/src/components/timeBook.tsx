c2b061740f8aa36642de992a7dad555800e14c0f
Michael Vogt <michael.vogt@progwise.net>
2021-04-20 13:10:59 +0200

    added end time

--- frontend/src/components/timeBook.tsx
+++ frontend/src/components/timeBook.tsx
@@ -9,9 +9,17 @@ export const TimeBook = (props: ITimeBookProps) => {
         'T': 3.0,
     };
 
+    const addDays = (date: Date, numDays: number) => {
+        const result = new Date(date);
+        result.setDate(result.getDate() + 6);
+        return result;
+    }
+
+    const endDay = addDays(props.startDay, 6);
+
     return (
         <>
-        <h1>Start day {props.startDay.toDateString()}</h1>
+        <h2>From {props.startDay.toDateString()} until {endDay.toDateString()}</h2>
         <table>
         <thead>
             <tr>
