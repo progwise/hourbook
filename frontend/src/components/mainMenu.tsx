627ebf1d894b1d2579b9f22e7837b078092f81d8
Armin Köllner <ARKoellner@googlemail.com>
2021-04-20 11:24:26 +0200

    Hmm...

--- frontend/src/components/mainMenu.tsx
+++ frontend/src/components/mainMenu.tsx
@@ -1,7 +1,13 @@
-export const MainMenu = () => (
-    <ul>
+export const MainMenu = () => {
+
+    let dayInfo = new Date();
+
+    setTimeout(() => dayInfo = new Date())
+
+    return (<ul>
+        <li>{dayInfo.toLocaleTimeString()}</li>
         <li>Time</li>
         <li>Projects</li>
         <li>Reports</li>
-    </ul>
-)
+    </ul>)
+}
