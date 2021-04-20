cd5f3af65ed817435b9861a7a5047153d1586680
HeikeKruger <heikrueg@gmail.com>
2021-04-20 14:52:32 +0200

    add projects

--- frontend/src/components/projectList.tsx
+++ frontend/src/components/projectList.tsx
@@ -1,28 +1,69 @@
-export const ProjectList = () => {
+export interface IPropsProjectList {
+    selecedProjectId?: number
+}
+
+export const ProjectList = (props: IPropsProjectList) => {
 
     // 1. array with 3 example projects plus project TS type
-    const projectData = [];
+    const projectData = [{
+        id: 1001,
+        name: 'TestProjekt',
+        start: new Date('2020-12-17T03:24:00'),
+        end: new Date('2021-12-17T03:24:00'),
+    },
+    {
+        id: 1002,
+        name: 'TestProjekt2',
+        start: new Date('2020-12-17T03:24:00'),
+        end: new Date('2021-12-17T03:24:00'),
+    },
+    {
+        id: 1003,
+        name: 'TestProjekt3',
+        start: new Date('2020-12-17T03:24:00'),
+        end: new Date('2021-12-17T03:24:00'),
+
+    }];
 
     // 2. prop with selected project id
 
     // 3. column with active | future | archived
 
+    const renderProject = (project: any, index: number) => {
+        if (props.selecedProjectId === project.id) {
+            return (
+                <tr key={index}>
+                    <td>{project.id}</td>
+                    <td>'Selected ' {project.name}</td>
+                    <td>{project.start.toDateString()}</td>
+                    <td>{project.end.toDateString()}</td>
+                </tr>)
+        }
+        return (
+            <tr key={index}>
+                <td>{project.id}</td>
+                <td>{project.name}</td>
+                <td>{project.start.toDateString()}</td>
+                <td>{project.end.toDateString()}</td>
+            </tr>)
+
+    }
+
     return <table>
         <thead>
-        <tr>
-            <th>No</th>
-            <th>Name</th>
-            <th>Start</th>
-            <th>Ende</th>
-        </tr>
+            <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Start</th>
+                <th>Ende</th>
+            </tr>
         </thead>
         <tbody>
-        <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-        </tr>
+            {
+                projectData.map((value, index) => {
+                    return renderProject(value, index)
+                })
+            }
         </tbody>
         <tfoot></tfoot>
     </table>
