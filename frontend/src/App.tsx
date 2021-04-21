67c20e2cac57cfc90ca9095f07bbcfc535ab009a
Christian Ma <christian.ma@telekom.de>
2021-04-21 11:11:30 +0200

    had to update .gitignore to not push node modules and others

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,6 @@ import {MainMenu} from './components/mainMenu';
 import {TimeBook} from './components/timeBook/timeBook';
 import {useState} from "react";
 import {IProject} from "./interfaces/iProject";
-import {LogoGadget} from "./components/logoGadget";
 
 function App() {
 
@@ -19,7 +18,6 @@ function App() {
 
     return (
         <>
-            <LogoGadget />
             <MainMenu currentProject={selectedProject}/>
             <UserSettings/>
             <ProjectList project={selectedProject} onProjectSelected={setSelectedProject}/>
