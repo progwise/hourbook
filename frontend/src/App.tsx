b08549858bc3aaee8f130bbda5f916b5789ad2e3
Michael Vogt <michael.vogt@progwise.net>
2021-04-21 13:51:15 +0200

    installed routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import { UserSettings } from "./components/userSettings";
 import { ProjectList } from './components/projectList';
 import { MainMenu } from './components/mainMenu';
 import { Logo } from './components/logo';
 import { TimeBook } from './components/timeBook/timeBook';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { IProject } from './common/definitions';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
 
@@ -18,13 +19,31 @@ function App() {
     const [selectedProject, setSelectedProject] = useState(p);
 
     return (
-        <>
+        <BrowserRouter>
             <Logo />
             <MainMenu project={selectedProject} />
-            <UserSettings />
-            <ProjectList selectedProjectId={selectedProject.id} onProjectSelected={setSelectedProject} />
-            <TimeBook />
-        </>
+            <Switch>
+                <Route path="/about">
+                    <h2>about</h2>
+                </Route>
+                <Route path="/usersettings">
+                    <h2>User Settings</h2>
+                    <UserSettings />
+                </Route>
+                <Route path="/projects">
+                    <div>Projects</div>
+                    <ProjectList selectedProjectId={selectedProject.id} onProjectSelected={setSelectedProject} />
+                </Route>
+                <Route path='/timebook'>
+                    <h2>Timebook</h2>
+                    <TimeBook />
+                </Route>
+                <Route path='*'>
+                    <h2>Homepage</h2>
+                    <p>Welcome to hourlist</p>
+                </Route>
+            </Switch>
+        </BrowserRouter>
     );
 }
 
