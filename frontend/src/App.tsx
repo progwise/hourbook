56840138b125cf9815a36bca099ce195dc654abf
Michael Vogt <michael.vogt@progwise.net>
2021-04-22 10:06:17 +0200

    project edit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import { UserSettings } from "./components/userSettings";
 import { ProjectList } from './components/projectList';
+import { EditProject } from './components/editProject';
 import { MainMenu } from './components/mainMenu';
 import { Logo } from './components/logo';
 import { TimeBook } from './components/timeBook/timeBook';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { IProject } from './common/definitions';
 
 function App() {
@@ -23,6 +24,9 @@ function App() {
             <MainMenu project={selectedProject} />
             <UserSettings />
             <ProjectList selectedProjectId={selectedProject.id} onProjectSelected={setSelectedProject} />
+            <hr></hr>
+            <EditProject></EditProject>
+            <hr></hr>
             <TimeBook />
         </>
     );
